86b158397ab70543ff88ca5177b4a98152c546e8
Andrea Massignan <73525197+duchannes19@users.noreply.github.com>
2024-05-10 15:35:17 +0200

    Just some prints

--- App/app.py
+++ App/app.py
@@ -14,21 +14,31 @@ tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 print('Loading model...')
-# Construct the path to the trained model file
+
+# Construct the path to the trained model file and load it
 model_path = os.path.join(current_directory, 'url_classifier.pth')
 model = load_model(model_path)
+
 print('Model loaded successfully!')
+
+# Set the model to evaluation mode
 model.eval()
+
+print('Model is in evaluation mode!')
 
 # Define a route to receive URL requests
 @app.route('/analyze_url', methods=['POST'])
 def analyze_url():
     # Get the URL from the request
     url = request.json.get('url')
+
+    print(f'Analyzing URL: {url}')
     
-    # Preprocess the URL and tokenize it using the BERT tokenizer
+    # Preprocess the URL and tokenize it using the BERT tokenizer not modified
     tokens = tokenizer.encode_plus(url, add_special_tokens=True, max_length=512, truncation=True, padding='max_length', return_tensors='pt')
     
+    print('URL tokenized successfully!')
+
     # Perform inference using the model
     with torch.no_grad():
         outputs = model(tokens['input_ids'])
@@ -39,6 +49,8 @@ def analyze_url():
     # Map the class label to a human-readable prediction
     class_labels = ['benign', 'phishing', 'defacement', 'malware']
     prediction = class_labels[predicted.item()]
+
+    print(f'Prediction: {prediction}')
     
     # Return the prediction as JSON response
     return jsonify({'prediction': prediction})
